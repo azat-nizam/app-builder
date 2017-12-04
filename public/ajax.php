8a92befd33c8d73c06d39faddd34db18a6b20df0
Azat Nizameev <azat@reaspekt.ru>
2017-12-04 13:37:12 +0300

    Initial commit

---
+++ public/ajax.php
@@ -0,0 +1,2 @@
+<?php
+print('ajax responce');
---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+/* Simple example for axios package */
+/*
+var axios = require('axios');
+var respContainer = document.getElementById('response');
+
+document.getElementById('btnAction').addEventListener('click', function() {
+	
+	axios.get('ajax.php').then(function(response) {
+		respContainer.innerHTML = response.data;
+	});
+});
+*/
